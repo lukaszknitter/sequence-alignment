73565eedf7ce7625a9f70f4caca0f1aa2e2df280
Łukasz Knitter <lukas.tt@wp.pl>
2020-01-17 19:38:32 +0100

    Exercise 1 cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,23 @@
 import algorithms.SequenceGlobalAlignment;
-import algorithms.SequenceGlobalAlignmentWithPenalty;
 import algorithms.SequenceLocalAlignment;
 
 import java.io.IOException;
 
 /**
  * Bioinformatyka
- * Optymalne zestawienia 2 sekwencji (I + II)
- * 
+ * Optymalne zestawienia 2 sekwencji I
+ *
  * @author Martyna Gostomska 160408
  * @author Łukasz Knitter 160246
  * @author Tomasz Kononowicz 160839
-**/
+ **/
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        SequenceGlobalAlignment global = new SequenceGlobalAlignment("resources//test_global.txt");
-        global.compute();
+	public static void main(String[] args) throws IOException {
+		SequenceGlobalAlignment global = new SequenceGlobalAlignment("resources//test_global.txt");
+		global.compute();
 
-        SequenceLocalAlignment local = new SequenceLocalAlignment("resources//test_local.txt");
-        local.compute();
-
-        SequenceGlobalAlignmentWithPenalty globalWithPenalty = new SequenceGlobalAlignmentWithPenalty("resources//test_penalty2.txt");
-        globalWithPenalty.compute();
-    }
+		SequenceLocalAlignment local = new SequenceLocalAlignment("resources//test_local.txt");
+		local.compute();
+	}
 }
