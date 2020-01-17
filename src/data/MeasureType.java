13928ea8d148e58a41e650aacddf4146947feebe
Łukasz Knitter <lukas.tt@wp.pl>
2020-01-17 19:37:13 +0100

    Exercise 2 cleanup

--- src/data/MeasureType.java
+++
@@ -1,6 +0,0 @@
-package data;
-
-public enum MeasureType {
-	DISTANCE,
-	SIMILARITY
-}
--- src/utils/SequenceUtils.java
+++ src/utils/SequenceUtils.java
@@ -1,37 +1,11 @@
 package utils;
 
-import feature.AminoAcidsTypes;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SequenceUtils {
-	public static String convertSequence(String sequence) {
-		StringBuilder convertedSequence = new StringBuilder();
-		for (int i = 0; i < sequence.length(); i += 3) {
-			convertedSequence.append(AminoAcidsTypes.valueOf(sequence.substring(i, i + 3)).getAminoAcid());
-		}
-		return convertedSequence.toString();
-	}
-
-	public static String reTranslateSequence(String sequence) {
-		StringBuilder convertedSequence = new StringBuilder();
-		for (int i = 0; i < sequence.length(); i++) {
-			convertedSequence.append(getAminoAcidCodon(sequence.substring(i, i + 1)));
-		}
-		return convertedSequence.toString();
-	}
-
-	public static String getAminoAcidCodon(String aminoAcid) {
-		for (AminoAcidsTypes aminoAcidType : AminoAcidsTypes.values()) {
-			if (aminoAcid.equals(aminoAcidType.getAminoAcid())) {
-				return aminoAcidType.toString();
-			}
-		}
-		return "   ";
-	}
 
 	public static ArrayList<String> getDistinctFromStrings(String first, String second) {
 		ArrayList<String> result = new ArrayList<>();
